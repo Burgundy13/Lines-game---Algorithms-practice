1370b549a0d91b9512b180d2476b5b2de13fea3c
Vlado Ivkovic <ivkovicv90@gmail.com>
2024-02-06 23:49:38 +0100

    Selection balls added

--- main.js
+++ main.js
@@ -3,12 +3,50 @@ let ballColors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'cyan'];
 
 makeGrid();
 let boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => box.addEventListener('click', selectBall));
 makeBalls(10);
+
+function selectBall() {
+	if (this.innerHTML !== '') {
+		findAllPossiblePaths(this);
+	} else {
+		alert('Selection must be ball');
+	}
+}
+
+function findAllPossiblePaths(selectedBall) {
+	let boxId = parseInt(selectedBall.id);
+	let leftBox = boxes[boxId - 1];
+	let rightBox = boxes[boxId + 1];
+	let topBox = boxes[boxId - 10];
+	let bottomBox = boxes[boxId + 10];
+
+	//left
+	if (boxId % 10 !== 0 && leftBox.innerHTML === '') {
+		leftBox.style.background = '#ddd';
+	}
+
+	//right
+	if (boxId % 10 !== 9 && rightBox.innerHTML === '') {
+		rightBox.style.background = '#ddd';
+	}
+
+	//top
+
+	if (topBox && topBox.innerHTML === '') {
+		topBox.style.background = '#ddd';
+	}
+
+	//bottom
+	if (bottomBox && bottomBox.innerHTML === '') {
+		bottomBox.style.background = '#ddd';
+	}
+}
 
 function makeGrid() {
 	let text = '';
 	for (let i = 0; i < 100; i++) {
-		text += `<div class="box" ></div>`.trim();
+		text += `<div class="box" id="${i}"></div>`.trim();
 	}
 
 	container.innerHTML = text;
